1fc8025dfa73177f4a2e1142007533276ffadcc7
Mark Hollomon <mhhollomon@gmail.com>
2025-11-09 10:00:20 -0500

    Fix up build and cli

--- src/SimpleTextMarkup/cli.py
+++ src/SimpleTextMarkup/cli.py
@@ -5,17 +5,17 @@ from argparse import ArgumentParser
 
 def gen_arg_parser():
     parser = ArgumentParser()
-    parser.add_argument('input', type=str, help='Input file')
-    parser.add_argument('output', type=str, help='Output file')
+    parser.add_argument('input', type=str, nargs='?', default=None, help='Input file - if not given, reads from stdin')
+    parser.add_argument('output', type=str, nargs='?', default=None, help='Output file - if not given, writes to stdout')
     return parser
 
 def main():
     args = gen_arg_parser().parse_args()
     input = args.input
     output = args.output
-    if not args.input :
+    if input is None or input == '-':
         input = sys.stdin
-    if not args.output :
+    if output is None or output == '-':
         output = sys.stdout
 
     stm_convert_to_file(input, output)
