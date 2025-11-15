ffb72b4145fea3b35bc533e5b75f4c793b6ac571
Mark Hollomon <mhhollomon@gmail.com>
2025-11-14 19:32:00 -0500

    start

--- src/SimpleTextMarkup/convert.py
+++ src/SimpleTextMarkup/convert.py
@@ -12,19 +12,10 @@ def default_options() -> Dict[str, Option]:
 # STMConverter
 ######################################################################
 class STMConverter(ParserProxy):
-    def __init__(self, src : LineSrc, formatters : list[FormatterBase] | None = None, options : dict[str, str] | None = None):
+    def __init__(self, src : LineSrc,  options : dict[str, str] | None = None):
         self.stack : list[Context] = []
         self.output = ''
         self.src = src
-
-        if formatters is not None:
-            self.formatters = formatters
-        else:
-            self.formatters = get_formatters(self)
-
-        self.embeds = [f for f in self.formatters if f.ftype == FormatterType.EMBEDDED]
-        self.oneliners = [f for f in self.formatters if f.ftype == FormatterType.ONELINE]
-        self.blocks = [f for f in self.formatters if f.ftype == FormatterType.BLOCK]
 
         self.options = default_options()
         if options is not None:
@@ -69,184 +60,54 @@ class STMConverter(ParserProxy):
         else:
             self.output += line
 
-    def parseEmbeddedFormatters(self) -> bool:
-        line = self.src.get_next()
-        if line is None:
-            return False
-
-        print(f"-- Input: {line}")
-        count = 0
-
-        while(len(line) > 0):
-            count += 1
-            print(f"-- Count: {count} stack: {len(self.stack)}")
-            if count > 100 :
-                raise Exception("Infinite loop")
-
-            # Nesting is controlled by the group name.
-            # Only one formatter of a group can be active.
-            in_progress = [c.fmt.group for c in self.stack if c.ftype == FormatterType.EMBEDDED]
-            # Reversed to make sure the inner most contexts are first in the list
-            terminated = {c.name : c.fmt for c in reversed(self.stack) if c.ftype == FormatterType.EMBEDDED}
-            possible = {f.name : f for f in self.embeds if f.group not in in_progress}
-
-            print(f"-- In progress: {in_progress}")
-            #print(f"-- Possible: {possible}")
-            print(f"-- Terminated: {terminated}")
-
-            regexes = []
-
-            ctx = self._inner_context()
-            #print(f"-- Context: {ctx}")
-            if ctx.ftype == FormatterType.BLOCK or ctx.fmt.nestable():
-                #print(" -- Nesting is allowed")
-                for starter, formatter in possible.items():
-                    r = formatter.start_re()
-                    regexes.append(f"(?P<{formatter.name}>{r})")
-
-            for ender, formatter in terminated.items():
-                r = formatter.end_re()
-                regexes.append(f"(?P<{formatter.name}>{r})")
-
-            regex_string = '|'.join(regexes)
-            print(f"-- Regex: {regex_string}")
-
-            final_regex = re.compile(regex_string)
-
-            m = final_regex.search(line)
-            if m:
-                # gotta be a better way
-                opener = m.group(0)
-                matching_formatter = [ i[0] for i in m.groupdict().items() if i[1] == opener ][0]
-                print(f"-- Matching formatter: {matching_formatter}")
-                print(f"-- Match: {opener}")
-                preamble = line[:m.start()]
-                print(f"-- Preamble: {preamble}")
-                line = line[m.end():]
-                self._inner_context().add_to_buffer(preamble)
-
-                if matching_formatter in possible :
-                    fm = possible[matching_formatter]
-                    if fm.self_closing():
-                        self._inner_context().add_to_buffer(fm.build_output(opener, opener))
-                    else :
-                        self._push_context(fm, opener)
-
-                elif matching_formatter in terminated:
-                    while True :
-                        if not self._in_context() or self._inner_context().ftype != FormatterType.EMBEDDED:
-                            break
-                        context = self._pop_context()
-                        if context.name != matching_formatter:
-                            self._inner_context().add_to_buffer(context.opener + context.buffer)
-                        else:
-                            self._close(context)
-                            break
-
+    def _skip_blanks_lines(self) -> bool:
+        while True :
+            line = self.src.get_next()
+            if line is None:
+                return False
+            if line.strip() == '':
                 continue
-
-            print(f"-- No match: {line}")
-            self._inner_context().add_to_buffer(line)
-            # short circuit - no need to continue looking
+            self.src.push_back(line)
             return True
-
-        return True
 
     def parseConfigs(self) -> bool:
-        line = self.src.get_next()
-        while (line is not None and line.strip() == ''):
-            line = self.src.get_next()
-        if line is None:
+
+        if not self._skip_blanks_lines() :
             return False
 
-        # for now, just ignore configs
-        self.src.push_back(line)
-        return False
+        line : str = self.src.get_next() # type: ignore
 
-    def _build_oneliner_regex(self) -> re.Pattern:
-        regexes = []
-        for f in self.oneliners:
-            r = f.start_re()
-            regexes.append(f"(?P<{f.name}>{r})")
-        return re.compile('|'.join(regexes))
-
-    def parseOneLiners(self) -> bool:
-        line = self.src.get_next()
-        while (line is not None and line.strip() == ''):
-            line = self.src.get_next()
-        if line is None:
+        if not line.startswith('%config '):
+            self.src.push_back(line)
             return False
 
-        regex = self._build_oneliner_regex()
-        print("-- One liner Regex: " + regex.pattern)
-        print("-- One liner line: " + line)
-        m = regex.search(line)
-        if m:
-            opener = m.group(0)
-            matching_formatter = [ i[0] for i in m.groupdict().items() if i[1] == opener ][0]
-            formatter = [ f for f in self.oneliners if f.name == matching_formatter ][0]
-            postlude = line[m.end():]
-            self._add_output(formatter.build_output(postlude, opener))
+        line = line[len('%config '):].strip()
 
-            return True
-
-        self.src.push_back(line)
-        return False
-
-    def parseBlock(self) -> bool:
-        line = self.src.get_next()
-        while (line is not None and line.strip() == ''):
-            line = self.src.get_next()
-        if line is None:
-            return False
-
-        self.src.push_back(line)
-
-        ctx = self._para_context()
-        self._push_context(ctx)
-
-        while not self.src.is_blank():
-            self.parseEmbeddedFormatters()
-
-        while self._in_context() and self._inner_context() != ctx:
-            self._close(self._pop_context())
-
-        self._close(self._pop_context())
+        for opt in re.findall(r'(\w+)=(.*)', line):
+            if opt[0] not in self.options:
+                raise Exception(f"Unknown option: {opt[0]}")
+            self.options[opt[0]].set_value(opt[1], OptionSource.FILE)
 
         return True
 
-    def _close(self, ctx : Context):
-        """Assumes ctx has already been popped from the stack"""
+    def parseBlock(self) -> bool:
+        if not self._skip_blanks_lines() :
+            return False
 
-        output = ctx.fmt.build_output(ctx.buffer.rstrip(), ctx.opener)
-        self._add_output(output)
+        line : str = self.src.get_next() # type: ignore
 
-    def _para_context(self) -> Context:
-        para_formatter = [ f for f in self.blocks if f.name == 'paragraph' ][0]
-        return Context(
-            fmt=para_formatter,
-            name=para_formatter.name,
-            ftype=para_formatter.ftype,
-            buffer='',
-            opener=''
-        )
+        return False
+
 
     def convert(self) -> str:
 
         if self.src is None:
             raise Exception("No input")
 
-        while (not self.src.is_at_end()):
+        while not self.src.is_at_end():
             while self.parseConfigs() :
                 pass
 
-            while self.parseOneLiners() :
-                pass
-
             self.parseBlock()
-
-
-        while self._in_context():
-            self._close(self._pop_context())
 
         return self.output
--- src/SimpleTextMarkup/impl_/linesrc.py
+++ src/SimpleTextMarkup/impl_/linesrc.py
@@ -4,13 +4,13 @@ from typing import Any, List, TextIO
 
 class LineSrc:
     def __init__(self, input : str | Path | TextIO | List[str]):
-        print(f"--- LineSrc:Input type: {type(input)}")
         self.pushback : List[str] = []
         self.data : List[str] = []
 
         self.index = 0
         self.io : Any = None
         self.at_end = False
+
         if isinstance(input, str):
             self.data = input.splitlines()
         elif isinstance(input, Path):
@@ -22,55 +22,40 @@ class LineSrc:
 
     def get_next(self) -> str | None:
         if self.at_end:
-            print("--- LineSrc: get_next() : End of input")
             return None
 
         if len(self.pushback) > 0:
-            print("--- LineSrc: get_next() : Returning pushed back line")
-            return self.pushback.pop(0)
+            return self.pushback.pop()
 
         if self.io is not None:
             line = self.io.readline()
             if line == '':
                 self.at_end = True
-                print("--- LineSrc: get_next() : IO returned EOF")
                 return None
             elif line[-1] == '\n':
-                print("--- LineSrc: get_next() : IO returning chomped line")
                 return line[:-1] + ' '
             else :
-                print("--- LineSrc: get_next() : IO returning line")
                 return line + ' '
 
         if self.index >= len(self.data):
-            print("--- LineSrc: Index - End of input")
             self.at_end = True
             return None
         self.index += 1
-        print(f"--- LineSrc: get_next() : Returning line '{self.data[self.index - 1]}'")
         return self.data[self.index - 1] + ' '
 
     def push_back(self, line : str):
-        print("--- LineSrc: push_back()")
         self.at_end = False
         self.pushback.append(line)
 
     def peek(self) -> str | None:
-        if self.at_end:
-            return None
 
-        if len(self.pushback) > 0:
-            return self.pushback[0]
+        line = self.get_next()
+        if line is not None:
+            self.push_back(line)
+        return line
 
-        if self.index >= len(self.data):
-            return None
-
-        return self.data[self.index] + ' '
 
     def is_blank(self) -> bool:
-        if self.at_end:
-            return True
-
         l = self.peek()
         return l is None or l.strip() == ''
 
